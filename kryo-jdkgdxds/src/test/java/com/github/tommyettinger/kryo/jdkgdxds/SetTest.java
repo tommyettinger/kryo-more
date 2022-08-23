218cd6c341a6d49f2fa242873ce0a4b251d799b4
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-08-22 20:08:54 -0700

    IntSet/LongSet. Test unordered sets.

---
+++ kryo-jdkgdxds/src/test/java/com/github/tommyettinger/kryo/jdkgdxds/SetTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2022 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.github.tommyettinger.kryo.jdkgdxds;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.github.tommyettinger.ds.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+
+public class SetTest {
+    @Test
+    public void testIntSet() {
+        Kryo kryo = new Kryo();
+        kryo.register(IntSet.class, new IntSetSerializer());
+
+        IntSet data = IntSet.with(-123, 0, 456, 0, 1, -1, 0);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(32);
+        Output output = new Output(baos);
+        kryo.writeObject(output, data);
+        byte[] bytes = output.toBytes();
+        try (Input input = new Input(bytes)) {
+            IntSet data2 = kryo.readObject(input, IntSet.class);
+            Assert.assertEquals(data, data2);
+        }
+    }
+
+    @Test
+    public void testLongSet() {
+        Kryo kryo = new Kryo();
+        kryo.register(LongSet.class, new LongSetSerializer());
+
+        LongSet data = LongSet.with(-1234567890L, 0L, 4567890123456789L, 0, 1L, 1, -1, 0);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(32);
+        Output output = new Output(baos);
+        kryo.writeObject(output, data);
+        byte[] bytes = output.toBytes();
+        try (Input input = new Input(bytes)) {
+            LongSet data2 = kryo.readObject(input, LongSet.class);
+            Assert.assertEquals(data, data2);
+        }
+    }
+
+    @Test
+    public void testObjectSet() {
+        Kryo kryo = new Kryo();
+        kryo.register(String.class);
+        kryo.register(ObjectSet.class);
+
+        ObjectSet<String> data = ObjectSet.with("Hello", "World", "!", "I", "am", "a", "test", "!");
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(32);
+        Output output = new Output(baos);
+        kryo.writeObject(output, data);
+        byte[] bytes = output.toBytes();
+        try (Input input = new Input(bytes)) {
+            ObjectSet<?> data2 = kryo.readObject(input, ObjectSet.class);
+            Assert.assertEquals(data, data2);
+        }
+    }
+
+}
