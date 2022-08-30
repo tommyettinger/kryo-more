e46fd72a16bcf4caac8e5fb88c8e89dbf151474a
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-08-29 22:15:08 -0700

    Four more!

---
+++ kryo-juniper/src/main/java/com/github/tommyettinger/kryo/juniper/distribution/ErlangDistributionSerializer.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2022 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.github.tommyettinger.kryo.juniper.distribution;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.github.tommyettinger.random.EnhancedRandom;
+import com.github.tommyettinger.random.distribution.ErlangDistribution;
+
+public class ErlangDistributionSerializer extends Serializer<ErlangDistribution> {
+    public ErlangDistributionSerializer() {
+        super(false, false);
+    }
+
+    @Override
+    public ErlangDistribution copy(Kryo kryo, ErlangDistribution original) {
+        return original.copy();
+    }
+
+    @Override
+    public void write(Kryo kryo, Output output, ErlangDistribution object) {
+        kryo.writeClassAndObject(output, object.generator);
+        output.writeVarInt((int) object.getParameterA(), true);
+        output.writeDouble(object.getParameterB());
+    }
+
+    @Override
+    public ErlangDistribution read(Kryo kryo, Input input, Class<? extends ErlangDistribution> type) {
+        return new ErlangDistribution((EnhancedRandom) kryo.readClassAndObject(input),
+                input.readVarInt(true), input.readDouble());
+    }
+}
