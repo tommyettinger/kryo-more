c6edddb487c138f61cc297f0e497d5dbed8b5163
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-10-08 17:07:11 -0700

    Update juniper dep; Pasar and Arcsine added.

---
+++ kryo-juniper/src/main/java/com/github/tommyettinger/kryo/juniper/PasarRandomSerializer.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2022 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.github.tommyettinger.kryo.juniper;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.github.tommyettinger.random.PasarRandom;
+
+/**
+ * Kryo {@link Serializer} for juniper {@link PasarRandom}s.
+ */
+public class PasarRandomSerializer extends Serializer<PasarRandom> {
+
+    public PasarRandomSerializer() {
+        setImmutable(false);
+        setAcceptsNull(false);
+    }
+
+    @Override
+    public void write(final Kryo kryo, final Output output, final PasarRandom data) {
+        output.writeVarLong(data.getStateA(), false);
+        output.writeVarLong(data.getStateB(), false);
+        output.writeVarLong(data.getStateC(), false);
+        output.writeVarLong(data.getStateD(), false);
+        output.writeVarLong(data.getStateE(), false);
+    }
+
+    @Override
+    public PasarRandom read(final Kryo kryo, final Input input, final Class<? extends PasarRandom> dataClass) {
+        return new PasarRandom(input.readVarLong(false), input.readVarLong(false),
+                input.readVarLong(false), input.readVarLong(false), input.readVarLong(false));
+    }
+
+    @Override
+    public PasarRandom copy(Kryo kryo, PasarRandom original) {
+        return original.copy();
+    }
+}
