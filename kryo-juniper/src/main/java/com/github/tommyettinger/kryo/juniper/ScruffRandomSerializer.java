28bec9d5763eb658abf1699bd70bc4c01fe7865a
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-05-23 15:04:39 -0700

    Start writing serializers for new juniper types.

---
+++ kryo-juniper/src/main/java/com/github/tommyettinger/kryo/juniper/ScruffRandomSerializer.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2022-2023 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.github.tommyettinger.kryo.juniper;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.github.tommyettinger.random.ScruffRandom;
+
+/**
+ * Kryo {@link Serializer} for juniper {@link ScruffRandom}s.
+ */
+public class ScruffRandomSerializer extends Serializer<ScruffRandom> {
+
+    public ScruffRandomSerializer() {
+        setImmutable(false);
+        setAcceptsNull(false);
+    }
+
+    @Override
+    public void write(final Kryo kryo, final Output output, final ScruffRandom data) {
+        output.writeVarLong(data.getStateA(), false);
+        output.writeVarLong(data.getStateB(), false);
+        output.writeVarLong(data.getStateC(), false);
+        output.writeVarLong(data.getStateD(), false);
+    }
+
+    @Override
+    public ScruffRandom read(final Kryo kryo, final Input input, final Class<? extends ScruffRandom> dataClass) {
+        return new ScruffRandom(input.readVarLong(false), input.readVarLong(false),
+                input.readVarLong(false), input.readVarLong(false));
+    }
+
+    @Override
+    public ScruffRandom copy(Kryo kryo, ScruffRandom original) {
+        return original.copy();
+    }
+}
