3280e0c4a3ec9b42ab5f5db29f9b1ad78e6cf2c6
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-03-01 22:18:19 -0800

    Add kryo-gand, for pathfinding.
    People have requested serializable graphs, and this is now a significantly more up-to-date version of the code than what kryo-simple-graphs uses.

---
+++ kryo-gand/src/main/java/com/github/tommyettinger/kryo/gand/DirectedGraphSerializer.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2022-2024 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.github.tommyettinger.kryo.gand;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.github.tommyettinger.gand.Connection;
+import com.github.tommyettinger.gand.DirectedGraph;
+
+import java.util.Collection;
+
+/**
+ * Kryo {@link Serializer} for gand {@link DirectedGraph}s.
+ * You should register the vertex type when you register this as the serializer for DirectedGraph.
+ */
+public class DirectedGraphSerializer extends Serializer<DirectedGraph<?>> {
+
+    public DirectedGraphSerializer() {
+        setAcceptsNull(false);
+    }
+
+    @Override
+    public void write(final Kryo kryo, final Output output, final DirectedGraph<?> data) {
+        Collection<?> vertices = data.getVertices();
+        Collection<? extends Connection<?>> edges = data.internals().getConnections();
+        int length = vertices.size();
+        output.writeInt(length, true);
+        for(Object v : vertices) {
+            kryo.writeClassAndObject(output, v);
+        }
+        length = edges.size();
+        output.writeInt(length, true);
+        for(Connection<?> e : edges) {
+            kryo.writeClassAndObject(output, e.getA());
+            kryo.writeClassAndObject(output, e.getB());
+            output.writeFloat(e.getWeight());
+        }
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked", "UnnecessaryLocalVariable"})
+    @Override
+    public DirectedGraph<?> read(final Kryo kryo, final Input input, final Class<? extends DirectedGraph<?>> dataClass) {
+        DirectedGraph<?> graph = new DirectedGraph<>();
+        DirectedGraph raw = graph;
+        int length = input.readInt(true);
+        for (int i = 0; i < length; i++) {
+            raw.addVertex(kryo.readClassAndObject(input));
+        }
+        length = input.readInt(true);
+        for (int i = 0; i < length; i++) {
+            raw.addEdge(kryo.readClassAndObject(input), kryo.readClassAndObject(input), input.readFloat());
+        }
+        return graph;
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked", "UnnecessaryLocalVariable"})
+    @Override
+    public DirectedGraph<?> copy(Kryo kryo, DirectedGraph<?> original) {
+        DirectedGraph<?> graph = new DirectedGraph<>();
+        DirectedGraph raw = graph;
+        Collection<?> vertices = graph.getVertices();
+        for(Object v : vertices){
+            raw.addVertex(kryo.copy(v));
+        }
+        Collection<? extends Connection<?>> edges = graph.internals().getConnections();
+        for(Connection<?> e : edges){
+            raw.addEdge(kryo.copy(e.getA()), kryo.copy(e.getB()), e.getWeight());
+        }
+        return graph;
+    }
+}
---
+++ kryo-gand/src/main/java/com/github/tommyettinger/kryo/gand/UndirectedGraphSerializer.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2022-2024 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.github.tommyettinger.kryo.gand;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.github.tommyettinger.gand.Connection;
+import com.github.tommyettinger.gand.UndirectedGraph;
+
+import java.util.Collection;
+
+/**
+ * Kryo {@link Serializer} for gand {@link UndirectedGraph}s.
+ * You should register the vertex type when you register this as the serializer for UndirectedGraph.
+ */
+public class UndirectedGraphSerializer extends Serializer<UndirectedGraph<?>> {
+
+    public UndirectedGraphSerializer() {
+        setAcceptsNull(false);
+    }
+
+    @Override
+    public void write(final Kryo kryo, final Output output, final UndirectedGraph<?> data) {
+        Collection<?> vertices = data.getVertices();
+        Collection<? extends Connection<?>> edges = data.internals().getConnections();
+        int length = vertices.size();
+        output.writeInt(length, true);
+        for(Object v : vertices) {
+            kryo.writeClassAndObject(output, v);
+        }
+        length = edges.size();
+        output.writeInt(length, true);
+        for(Connection<?> e : edges) {
+            kryo.writeClassAndObject(output, e.getA());
+            kryo.writeClassAndObject(output, e.getB());
+            output.writeFloat(e.getWeight());
+        }
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked", "UnnecessaryLocalVariable"})
+    @Override
+    public UndirectedGraph<?> read(final Kryo kryo, final Input input, final Class<? extends UndirectedGraph<?>> dataClass) {
+        UndirectedGraph<?> graph = new UndirectedGraph<>();
+        UndirectedGraph raw = graph;
+        int length = input.readInt(true);
+        for (int i = 0; i < length; i++) {
+            raw.addVertex(kryo.readClassAndObject(input));
+        }
+        length = input.readInt(true);
+        for (int i = 0; i < length; i++) {
+            raw.addEdge(kryo.readClassAndObject(input), kryo.readClassAndObject(input), input.readFloat());
+        }
+        return graph;
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked", "UnnecessaryLocalVariable"})
+    @Override
+    public UndirectedGraph<?> copy(Kryo kryo, UndirectedGraph<?> original) {
+        UndirectedGraph<?> graph = new UndirectedGraph<>();
+        UndirectedGraph raw = graph;
+        Collection<?> vertices = graph.getVertices();
+        for(Object v : vertices){
+            raw.addVertex(kryo.copy(v));
+        }
+        Collection<? extends Connection<?>> edges = graph.internals().getConnections();
+        for(Connection<?> e : edges){
+            raw.addEdge(kryo.copy(e.getA()), kryo.copy(e.getB()), e.getWeight());
+        }
+        return graph;
+    }
+}
