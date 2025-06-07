933533d1af6e89d994d3ada058c0a7727f1330f2
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-06-06 21:53:39 -0700

    Add some tests, they pass...

--- kryo-libgdx/src/test/java/com/github/tommyettinger/kryo/libgdx/GdxTest.java
+++ kryo-libgdx/src/test/java/com/github/tommyettinger/kryo/libgdx/GdxTest.java
@@ -17,6 +17,8 @@
 
 package com.github.tommyettinger.kryo.libgdx;
 
+import com.badlogic.gdx.math.Affine2;
+import com.badlogic.gdx.utils.ArrayMap;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
@@ -36,6 +38,36 @@ public class GdxTest {
         byte[] bytes = output.toBytes();
         try (Input input = new Input(bytes)) {
             Affine2 data2 = kryo.readObject(input, Affine2.class);
+            // Affine2 does not implement equals().
+//            Assert.assertEquals(data, data2);
+            Assert.assertEquals(data.m00, data2.m00, 0.00001f);
+            Assert.assertEquals(data.m01, data2.m01, 0.00001f);
+            Assert.assertEquals(data.m02, data2.m02, 0.00001f);
+            Assert.assertEquals(data.m10, data2.m10, 0.00001f);
+            Assert.assertEquals(data.m11, data2.m11, 0.00001f);
+            Assert.assertEquals(data.m12, data2.m12, 0.00001f);
+        }
+    }
+    @Test
+    public void testArrayMap() {
+        Kryo kryo = new Kryo();
+        kryo.register(ArrayMap.class, new ArrayMapSerializer());
+
+        ArrayMap<String, Integer> data = new ArrayMap<>();
+        data.put("Cthulhu", -123456);
+        data.put("lies", Integer.MIN_VALUE);
+        data.put("deep", 456789012);
+        data.put("in", 0);
+        data.put("Rl'yeh", 1111);
+        data.put("dreaming", 1);
+        data.put("of", -1);
+        data.put("waffles", 0);
+
+        Output output = new Output(32, -1);
+        kryo.writeObject(output, data);
+        byte[] bytes = output.toBytes();
+        try (Input input = new Input(bytes)) {
+            ArrayMap<?,?> data2 = kryo.readObject(input, ArrayMap.class);
             Assert.assertEquals(data, data2);
         }
     }
