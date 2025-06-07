ad5519cd7c31bc99af0ef2426a2456880a99fc62
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-06-06 22:53:49 -0700

    ByteArray...

--- kryo-libgdx/src/test/java/com/github/tommyettinger/kryo/libgdx/GdxTest.java
+++ kryo-libgdx/src/test/java/com/github/tommyettinger/kryo/libgdx/GdxTest.java
@@ -23,6 +23,7 @@ import com.badlogic.gdx.math.collision.BoundingBox;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ArrayMap;
 import com.badlogic.gdx.utils.BooleanArray;
+import com.badlogic.gdx.utils.ByteArray;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
@@ -144,6 +145,22 @@ public class GdxTest {
                     Assert.assertEquals(data.max, data2.max);
                 }
             }
+        }
+    }
+
+    @Test
+    public void testByteArray() {
+        Kryo kryo = new Kryo();
+        kryo.register(ByteArray.class, new ByteArraySerializer());
+
+        ByteArray data = ByteArray.with(new byte[]{-123, 0, 45, 0, 1, -1, 0});
+
+        Output output = new Output(32, -1);
+        kryo.writeObject(output, data);
+        byte[] bytes = output.toBytes();
+        try (Input input = new Input(bytes)) {
+            ByteArray data2 = kryo.readObject(input, ByteArray.class);
+            Assert.assertEquals(data, data2);
         }
     }
 }
