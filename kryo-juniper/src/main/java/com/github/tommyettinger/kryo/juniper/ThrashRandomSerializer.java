7ac4ecce8a9fe5273f3d8374acc3c8a86d5447c6
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-08-21 21:47:55 -0700

    ThrashRandomSerializer, much like Solo...

---
+++ kryo-juniper/src/main/java/com/github/tommyettinger/kryo/juniper/ThrashRandomSerializer.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2022-2024 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.github.tommyettinger.kryo.juniper;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.github.tommyettinger.random.ThrashRandom;
+
+/**
+ * Kryo {@link Serializer} for juniper {@link ThrashRandom}s.
+ */
+public class ThrashRandomSerializer extends Serializer<ThrashRandom> {
+
+    public ThrashRandomSerializer() {
+        setImmutable(false);
+        setAcceptsNull(false);
+    }
+
+    @Override
+    public void write(final Kryo kryo, final Output output, final ThrashRandom data) {
+        output.writeVarLong(data.getStateA(), false);
+        output.writeVarLong(data.getStateB(), false);
+        output.writeVarLong(data.getStateC(), false);
+    }
+
+    @Override
+    public ThrashRandom read(final Kryo kryo, final Input input, final Class<? extends ThrashRandom> dataClass) {
+        return new ThrashRandom(input.readVarLong(false), input.readVarLong(false), input.readVarLong(false));
+    }
+
+    @Override
+    public ThrashRandom copy(Kryo kryo, ThrashRandom original) {
+        return original.copy();
+    }
+}
